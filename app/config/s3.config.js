a336f005e8cac6c814590877c7f8e36445a73505
Ishan Gayantha <hackishmax321@gmail.com>
2024-06-15 15:30:01 +0530

    first commit ch5

--- app/config/s3.config.js
+++ app/config/s3.config.js
@@ -3,9 +3,9 @@ const AWS = require("aws-sdk");
 require("dotenv").config();
 
 const s3Client = new AWS.S3({
-  accessKeyId: AWS_ACCESS_KEY,
-  secretAccessKey: AWS_SECRET_ACCESS_KEY,
-  region: REGION,
+  accessKeyId: process.env.AWS_ACCESS_KEY,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  region: process.env.REGION,
 });
 
 const uploadParams = {
--- app/config/s3.env.js
+++ app/config/s3.env.js
@@ -1,7 +1,7 @@
 const env = {
-  AWS_ACCESS_KEY: AWS_ACCESS_KEY_2,
-  AWS_SECRET_ACCESS_KEY: AWS_SECRET_ACCESS_KEY_2,
-  REGION: REGION,
+  AWS_ACCESS_KEY: process.env.AWS_ACCESS_KEY_2,
+  AWS_SECRET_ACCESS_KEY: process.env.AWS_SECRET_ACCESS_KEY_2,
+  REGION: process.env.REGION,
   Bucket: "awsvermentorrii",
 };
 
